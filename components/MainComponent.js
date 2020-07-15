aa57cd88f77b2e4fa4769d53e9e49e2b1ec91ddb
Anwesa <anwesa2099@gmail.com>
2020-07-15 23:40:08 +0530

    Secure Store

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -14,6 +14,7 @@ import { connect } from 'react-redux';
 import { fetchDishes, fetchComments, fetchPromos, fetchLeaders } from '../redux/ActionCreators';
 import Reservation from './ReservationComponent';
 import Favorites from './FavoriteComponent';
+import Login from './LoginComponent';
 
 const mapStateToProps = state => {
   return {
@@ -30,6 +31,23 @@ const mapDispatchToProps = dispatch => ({
   fetchPromos: () => dispatch(fetchPromos()),
   fetchLeaders: () => dispatch(fetchLeaders()),
 })
+
+const LoginNavigator = createStackNavigator({
+      Login: { screen: Login }
+    }, {
+    navigationOptions: ({ navigation }) => ({
+      headerStyle: {
+          backgroundColor: "#512DA8"
+      },
+      headerTitleStyle: {
+          color: "#fff"            
+      },
+      headerTintColor: "#fff",
+      headerLeft: <Icon name="menu" size={24}
+        iconStyle={{ color: 'white' }} 
+        onPress={ () => navigation.toggleDrawer() } />    
+    })
+  });
 
 
 const HomeNavigator = createStackNavigator({
@@ -158,6 +176,21 @@ const CustomDrawerContentComponent = (props) => (
   );
 
 const MainNavigator = createDrawerNavigator({
+    Login: 
+  { screen: LoginNavigator,
+    navigationOptions: {
+      title: 'Login',
+      drawerLabel: 'Login',
+      drawerIcon: ({ tintColor, focused }) => (
+        <Icon
+          name='sign-in'
+          type='font-awesome'            
+          size={24}
+          iconStyle={{ color: tintColor }}
+        />
+      ),
+    }
+  },
     Home: 
       { screen: HomeNavigator,
         navigationOptions: {
@@ -251,6 +284,7 @@ const MainNavigator = createDrawerNavigator({
         }
       }
 }, {
+  initialRouteName: 'Home',
   drawerBackgroundColor: '#D1C4E9',
   contentComponent: CustomDrawerContentComponent
 });
